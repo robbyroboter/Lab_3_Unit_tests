95d036be4cec1e101b8e2e2c7f54db84d83ad96d
robbyroboter <rosticzuev2002@mail.ru>
2024-03-18 21:16:52 +0500

    Перенёс тесты в ArithmeticTest

--- src/test/java/com/example/DataProviderTest.java
+++ src/test/java/com/example/DataProviderTest.java
@@ -3,7 +3,6 @@ package com.example;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
